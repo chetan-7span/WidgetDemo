403df771735da8f06e11f53b3858057e0f5d917b
Chetan Hedamba <chetan.hedamba@coorb.io>
2025-01-09 10:22:36 +0530

    Initial Commit

---
+++ WidgetDemo/WidgetDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  WidgetDemoApp.swift
+//  WidgetDemo
+//
+//  Created by Chetan Hedamba on 09/01/25.
+//
+
+import SwiftUI
+
+@main
+struct WidgetDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
